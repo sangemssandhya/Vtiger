bc12b23e3691aee1561a05a92cac07abb36c7c36
DELL <DELL@DESKTOP-K82IFAH>
2024-03-12 13:26:38 +0530

    user 2 commit

--- src/test/java/testscripts/createContactTest.java
+++ src/test/java/testscripts/createContactTest.java
@@ -9,9 +9,9 @@ import genericLibraries.IConstantPath;
 
 public class createContactTest extends BaseClass {
 	@Test
-	public void createContactTest() throws InterruptedException
+	public void CreateContactTest() throws InterruptedException
 	{
-		home.clickContacts();
+		home.clickContacts();//naviagte to home page of contact
 		soft.assertEquals(contact.getPageHeader(),"Contacts");
 		contact.clickPlusButton();
 		soft.assertEquals(createContact.getPageHeader(),"Creating New Contact");
