b0feb788f93d5a97ce9fc03a4e902cfc231d9516
DELL <DELL@DESKTOP-K82IFAH>
2024-03-13 13:57:32 +0530

    jenkins parameterized

---
+++ src/test/java/mavenPractice/LaunchBrowserTest.java
@@ -0,0 +1,45 @@
+package mavenPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowserTest {
+	WebDriver driver=null;
+	String browser=System.getProperty("Browser");
+	String url=System.getProperty("URL");
+
+    @Test
+	public void launchBrowser() throws InterruptedException {
+		
+	
+	switch(browser)
+	{
+	case "chrome":
+		//System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		break;
+	case "firefox":
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+	case "edge":
+		WebDriverManager.edgedriver().setup();
+		driver=new EdgeDriver();
+		break;
+	default:
+		System.out.println("Invalid browser info");
+	
+	
+	}
+	driver.manage().window().maximize();
+	driver.get(url);
+	Thread.sleep(3000);
+	driver.close();
+	
+}
+}
